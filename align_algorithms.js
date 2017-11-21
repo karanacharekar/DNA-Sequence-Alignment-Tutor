dc352b8c6e34e7f73eb6b1e5e1891cab802dcf7a
Karan Acharekar <karancr7acharekar@gmail.com>
2017-11-21 02:31:18 -0500

    working global alignment function

--- align_algorithms.js
+++ align_algorithms.js
@@ -1,33 +1,50 @@
 
-var Align = (function () {
-  "use strict";
-   return {
-      global: (function (query,db,scoring_matrix,alpha,gap_penalty){
-	
-	var score_mat = [qury.length+1][db.length+1]
 
+    function global(query_seq,db_seq,matrix,alpha,gap_penalty){
+	console.log("insd global")
+	alpha = alpha.toUpperCase();
+
+	console.log(query_seq)
+	console.log(db_seq)
+
+	var row_len = query_seq.length+1
+	var col_len = db_seq.length+1
+	var score_mat = new Array(row_len).fill(null).map(()=>new Array(col_len).fill(null));
+	console.log(score_mat)
 	var i=0;
 	score_mat[0][0]=0;
-	for(int j=1;j<=db_seq.length();j++) {
+
+
+	for(var j=1;j<=db_seq.length;j++) {
 		score_mat[i][j] = score_mat[i][j-1] + gap_penalty; 
 		}
 					
 	var j=0;
-	for(i=1;i<=query_seq.length();i++) {
+	for(i=1;i<=query_seq.length;i++) {
 		score_mat[i][j] = score_mat[i-1][j] + gap_penalty; 
 		}
 
 
-	for(var m=1;m<query.length;m++){
-		for(var n=1;n<db.length;n++){
-
+	for(i=1;i<query_seq.length+1;i++){
+		for( j=1;j<db_seq.length+1;j++){
+			console.log(i+","+j)
 			var query_char = (query_seq.charAt(i-1)).toUpperCase();
 			var db_char = (db_seq.charAt(j-1)).toUpperCase();
+			//console.log(query_char)
+			//console.log(db_char)
 			var val = matrix[alpha.indexOf(query_char)][alpha.indexOf(db_char)];
+			//console.log(val)
+			
 			score_mat[i][j]= Math.max(Math.max(score_mat[i][j-1] + gap_penalty,score_mat[i-1][j]+gap_penalty),score_mat[i-1][j-1] + val);
 
 		}
+		
 	}
+
+	console.log(score_mat)
+	console.log("finalyy")
+	
+
 	var aligned_query = "";
 	var aligned_db = "";
 	i = query_seq.length;
@@ -37,9 +54,11 @@ var Align = (function () {
 	while(i!= 0 && j!=0) {
 		var temp = []
 		n = backtrack.length;
-		char query_char = Character.toUpperCase(query_seq.charAt(i-1));
-		char db_char = Character.toUpperCase(db_seq.charAt(j-1));
-		int val = matrix[alpha.indexOf(query_char)][alpha.indexOf(db_char)];
+		console.log(i+","+j)
+		console.log(backtrack);
+		var query_char = (query_seq.charAt(i-1)).toUpperCase();
+		var db_char = (db_seq.charAt(j-1)).toUpperCase();
+		var val = matrix[alpha.indexOf(query_char)][alpha.indexOf(db_char)];
 				
 		if(score_mat[i][j] == (score_mat[i-1][j-1] + val)) {
 			aligned_query = query_seq.charAt(i-1) + aligned_query;
@@ -49,14 +68,14 @@ var Align = (function () {
 			j--;
 		}
 					
-		if(score_mat[i][j] == (score_mat[i-1][j] + gap_penalty)) {
+		else if(score_mat[i][j] == (score_mat[i-1][j] + gap_penalty)) {
 			aligned_query = query_seq.charAt(i-1) + aligned_query;
 			aligned_db = "-" + aligned_db;
 			temp.push([i-1,j])
 			i--;
 		}
 		
-		if(score_mat[i][j] == (score_mat[i][j-1] + gap_penalty))  {
+		else if(score_mat[i][j] == (score_mat[i][j-1] + gap_penalty))  {
 			aligned_query = "-" + aligned_query;
 			aligned_db = db_seq.charAt(j-1) + aligned_db;
 			temp.push([i,j-1])
@@ -64,16 +83,9 @@ var Align = (function () {
 		}
 		backtrack.push(temp)
 		}
-		return backtrack;
-}()),
 
+		var return_res = [score_mat,backtrack]
+		return return_res;
+}
 
-
-      local: (function () {
-        return console.log('test 2');
-      })
-   };
-}());
-
-
-
+      
