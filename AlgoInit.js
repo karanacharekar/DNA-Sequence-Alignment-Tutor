59cbdfdc556745ee14c23a62e217a26c49e62e48
Karan Acharekar <karancr7acharekar@gmail.com>
2017-11-25 14:03:06 -0500

    init

--- AlgoInit.js
+++ AlgoInit.js
@@ -7,37 +7,137 @@ app.controller("initctrl", function($scope , $http){
 	var regexn = /[acgt][acgt][acgt][acgt]*/i;
 	var regexp = /[arndceqghilkmfpstwyv][arndceqghilkmfpstwyv][arndceqghilkmfpstwyv][arndceqghilkmfpstwyv]*/i;
 
-	var myObj = {"submatrixN":[["submat1" , [[1,-1,-1,-1] ,[-1,1,-1,-1],[-1,-1,1,-1],[-1,-1,-1,1]] ] ,["submat2" , [[1,-3,-3,-3] ,[-3,1,-3,-3],[-3,-3,1,-3],[-3,-3,-3,1]] ]], "submatrixP":[[ "PAM250" , [[13,6,9,9,5,8,9,12,6,8,6,7,7,4,11,11,11,2,4,9],[3,17,4,3,2,5,3,2,6,3,2,9,4,1,4,4,3,7,2,2],[4,4,6,7,2,5,6,4,6,3,2,5,3,2,4,5,4,2,3,3],[5,4,8,11,1,7,10,5,6,3,2,5,3,1,4,5,5,1,2,3],[2,1,1,1,52,1,1,2,2,2,1,1,1,1,2,3,2,1,4,2],[3,5,5,6,1,10,7,3,7,2,3,5,3,1,4,3,3,1,2,3],[5,4,7,11,1,9,12,5,6,3,2,5,3,1,4,5,5,1,2,3],[12,5,10,10,4,7,9,27,5,5,4,6,5,3,8,11,9,2,3,7],[2,5,5,4,2,7,4,2,15,2,2,3,2,2,3,3,2,2,3,2],[3,2,2,2,2,2,2,2,2,10,6,2,6,5,2,3,4,1,3,9],[6,4,4,3,2,6,4,3,5,15,34,4,20,13,5,4,6,6,7,13],[6,18,10,8,2,10,8,5,8,5,4,24,9,2,6,8,8,4,3,5],[1,1,1,1,0,1,1,1,1,2,3,2,6,2,1,1,1,1,1,2],[2,1,2,1,1,1,1,1,3,5,6,1,4,32,1,2,2,4,20,3],[7,5,5,4,3,5,4,5,5,3,3,4,3,2,20,6,5,1,2,4],[9,6,8,7,7,6,7,9,6,5,4,7,5,3,9,10,9,4,4,6],[8,5,6,6,4,5,5,6,4,6,4,6,5,3,6,8,11,2,3,6],[0,2,0,0,0,0,0,0,1,0,1,0,0,1,0,1,0,55,1,0],[1,1,2,1,3,1,1,1,3,2,2,1,2,15,1,2,2,3,31,2],[7,4,4,4,4,4,4,4,5,4,15,10,4,10,5,5,5,72,4,17]]] ,["BLOSUM45" ,[[-2,-1,-2,-1,-1,-1,0,-2,-1,-1,-1,-1,-2,-1,1,0,-2,-2,0],[-2,7,0,-1,-3,1,0,-2,0,-3,-2,3,-1,-2,-2,-1,-1,-2,-1,-2],[-1,0,6,2,-2,0,0,0,1,-2,-3,0,-2,-2,-2,1,0,-4,-2,-3],[-2,-1,2,7,-3,0,2,-1,0,-4,-3,0,-3,-4,-1,0,-1,-4,-2,-3],[-1,-3,-2,-3,12,-3,-3,-3,-3,-3,-2,-3,-2,-2,-4,-1,-1,-5,-3,-1],[-1,1,0,0,-3,6,2,-2,1,-2,-2,1,0,-4,-1,0,-1,-2,-1,-3],[-1,0,0,2,-3,2,6,-2,0,-3,-2,1,-2,-3,0,0,-1,-3,-2,-3],[0,-2,0,-1,-3,-2,-2,7,-2,-4,-3,-2,-2,-3,-2,0,-2,-2,-3,-3],[-2,0,1,0,-3,1,0,-2,10,-3,-2,-1,0,-2,-2,-1,-2,-3,2,-3],[-1,-3,-2,-4,-3,-2,-3,-4,-3,5,2,-3,2,0,-2,-2,-1,-2,0,3],[-1,-2,-3,-3,-2,-2,-2,-3,-2,2,5,-3,2,1,-3,-3,-1,-2,0,1],[-1,3,0,0,-3,1,1,-2,-1,-3,-3,5,-1,-3,-1,-1,-1,-2,-1,-2],[-1,-1,-2,-3,-2,0,-2,-2,0,2,2,-1,6,0,-2,-2,-1,-2,0,1],[-2,-2,-2,-4,-2,-4,-3,-3,-2,0,1,-3,0,8,-3,-2,-1,1,3,0],[-1,-2,-2,-1,-4,-1,0,-2,-2,-2,-3,-1,-2,-3,9,-1,-1,-3,-3,-3],[1,-1,1,0,-1,0,0,0,-1,-2,-3,-1,-2,-2,-1,4,2,-4,-2,-1],[0,-1,0,-1,-1,-1,-1,-2,-2,-1,-1,-1,-1,-1,-1,2,5,-3,-1,0],[-2,-2,-4,-4,-5,-2,-3,-2,-3,-2,-2,-2,-2,1,-3,-4,-3,15,3,-3],[-2,-1,-2,-2,-3,-1,-2,-3,2,0,0,-1,0,3,-3,-2,-1,3,8,-1],[0,-2,-3,-3,-1,-3,-3,-3,-3,3,1,-2,1,0,-3,-1,0,-3,-1,5]]] ,[ "BLOSUM62" , [[4,-1,-2,-2,0,-1,-1,0,-2,-1,-1,-1,-1,-2,-1,1,0,-3,-2,0],[-1,5,0,-2,-3,1,0,-2,0,-3,-2,2,-1,-3,-2,-1,-1,-3,-2,-3],[-2,0,6,1,-3,0,0,0,1,-3,-3,0,-2,-3,-2,1,0,-4,-2,-3],[-2,-2,1,6,-3,0,2,-1,-1,-3,-4,-1,-3,-3,-1,0,-1,-4,-3,-3],[0,-3,-3,-3,9,-3,-4,-3,-3,-1,-1,-3,-1,-2,-3,-1,-1,-2,-2,-1],[-1,1,0,0,-3,5,2,-2,0,-3,-2,1,0,-3,-1,0,-1,-2,-1,-2],[-1,0,0,2,-4,2,5,-2,0,-3,-3,1,-2,-3,-1,0,-1,-3,-2,-2],[0,-2,0,-1,-3,-2,-2,6,-2,-4,-4,-2,-3,-3,-2,0,-2,-2,-3,-3],[-2,0,1,-1,-3,0,0,-2,8,-3,-3,-1,-2,-1,-2,-1,-2,-2,2,-3],[-1,-3,-3,-3,-1,-3,-3,-4,-3,4,2,-3,1,0,-3,-2,-1,-3,-1,3],[-1,-2,-3,-4,-1,-2,-3,-4,-3,2,4,-2,2,0,-3,-2,-1,-2,-1,1],[-1,2,0,-1,-3,1,1,-2,-1,-3,-2,5,-1,-3,-1,0,-1,-3,-2,-2],[-1,-1,-2,-3,-1,0,-2,-3,-2,1,2,-1,5,0,-2,-1,-1,-1,-1,1],[-2,-3,-3,-3,-2,-3,-3,-3,-1,0,0,-3,0,6,-4,-2,-2,1,3,-1],[-1,-2,-2,-1,-3,-1,-1,-2,-2,-3,-3,-1,-2,-4,7,-1,-1,-4,-3,-2],[1,-1,1,0,-1,0,0,0,-1,-2,-2,0,-1,-2,-1,4,1,-3,-2,-2],[0,-1,0,-1,-1,-1,-1,-2,-2,-1,-1,-1,-1,-2,-1,1,5,-2,-2,0],[-3,-3,-4,-4,-2,-2,-3,-2,-2,-3,-2,-3,-1,1,-4,-3,-2,11,2,-3],[-2,-2,-2,-3,-2,-1,-2,-3,2,-1,-1,-2,-1,3,-3,-2,-2,2,7,-1],[0,-3,-3,-3,-1,-2,-2,-3,-3,3,1,-2,1,-1,-2,-2,0,-3,-1,4]] ] ]};
+	/*var myObj = {"submatrixN":[["submat1" , [[1,-1,-1,-1] ,[-1,1,-1,-1],[-1,-1,1,-1],[-1,-1,-1,1]] ] ,["submat2" , [[1,-3,-3,-3] ,[-3,1,-3,-3],[-3,-3,1,-3],[-3,-3,-3,1]] ]], "submatrixP":[[ "PAM250" , [[13,6,9,9,5,8,9,12,6,8,6,7,7,4,11,11,11,2,4,9],[3,17,4,3,2,5,3,2,6,3,2,9,4,1,4,4,3,7,2,2],[4,4,6,7,2,5,6,4,6,3,2,5,3,2,4,5,4,2,3,3],[5,4,8,11,1,7,10,5,6,3,2,5,3,1,4,5,5,1,2,3],[2,1,1,1,52,1,1,2,2,2,1,1,1,1,2,3,2,1,4,2],[3,5,5,6,1,10,7,3,7,2,3,5,3,1,4,3,3,1,2,3],[5,4,7,11,1,9,12,5,6,3,2,5,3,1,4,5,5,1,2,3],[12,5,10,10,4,7,9,27,5,5,4,6,5,3,8,11,9,2,3,7],[2,5,5,4,2,7,4,2,15,2,2,3,2,2,3,3,2,2,3,2],[3,2,2,2,2,2,2,2,2,10,6,2,6,5,2,3,4,1,3,9],[6,4,4,3,2,6,4,3,5,15,34,4,20,13,5,4,6,6,7,13],[6,18,10,8,2,10,8,5,8,5,4,24,9,2,6,8,8,4,3,5],[1,1,1,1,0,1,1,1,1,2,3,2,6,2,1,1,1,1,1,2],[2,1,2,1,1,1,1,1,3,5,6,1,4,32,1,2,2,4,20,3],[7,5,5,4,3,5,4,5,5,3,3,4,3,2,20,6,5,1,2,4],[9,6,8,7,7,6,7,9,6,5,4,7,5,3,9,10,9,4,4,6],[8,5,6,6,4,5,5,6,4,6,4,6,5,3,6,8,11,2,3,6],[0,2,0,0,0,0,0,0,1,0,1,0,0,1,0,1,0,55,1,0],[1,1,2,1,3,1,1,1,3,2,2,1,2,15,1,2,2,3,31,2],[7,4,4,4,4,4,4,4,5,4,15,10,4,10,5,5,5,72,4,17]]] ,["BLOSUM45" ,[[-2,-1,-2,-1,-1,-1,0,-2,-1,-1,-1,-1,-2,-1,1,0,-2,-2,0],[-2,7,0,-1,-3,1,0,-2,0,-3,-2,3,-1,-2,-2,-1,-1,-2,-1,-2],[-1,0,6,2,-2,0,0,0,1,-2,-3,0,-2,-2,-2,1,0,-4,-2,-3],[-2,-1,2,7,-3,0,2,-1,0,-4,-3,0,-3,-4,-1,0,-1,-4,-2,-3],[-1,-3,-2,-3,12,-3,-3,-3,-3,-3,-2,-3,-2,-2,-4,-1,-1,-5,-3,-1],[-1,1,0,0,-3,6,2,-2,1,-2,-2,1,0,-4,-1,0,-1,-2,-1,-3],[-1,0,0,2,-3,2,6,-2,0,-3,-2,1,-2,-3,0,0,-1,-3,-2,-3],[0,-2,0,-1,-3,-2,-2,7,-2,-4,-3,-2,-2,-3,-2,0,-2,-2,-3,-3],[-2,0,1,0,-3,1,0,-2,10,-3,-2,-1,0,-2,-2,-1,-2,-3,2,-3],[-1,-3,-2,-4,-3,-2,-3,-4,-3,5,2,-3,2,0,-2,-2,-1,-2,0,3],[-1,-2,-3,-3,-2,-2,-2,-3,-2,2,5,-3,2,1,-3,-3,-1,-2,0,1],[-1,3,0,0,-3,1,1,-2,-1,-3,-3,5,-1,-3,-1,-1,-1,-2,-1,-2],[-1,-1,-2,-3,-2,0,-2,-2,0,2,2,-1,6,0,-2,-2,-1,-2,0,1],[-2,-2,-2,-4,-2,-4,-3,-3,-2,0,1,-3,0,8,-3,-2,-1,1,3,0],[-1,-2,-2,-1,-4,-1,0,-2,-2,-2,-3,-1,-2,-3,9,-1,-1,-3,-3,-3],[1,-1,1,0,-1,0,0,0,-1,-2,-3,-1,-2,-2,-1,4,2,-4,-2,-1],[0,-1,0,-1,-1,-1,-1,-2,-2,-1,-1,-1,-1,-1,-1,2,5,-3,-1,0],[-2,-2,-4,-4,-5,-2,-3,-2,-3,-2,-2,-2,-2,1,-3,-4,-3,15,3,-3],[-2,-1,-2,-2,-3,-1,-2,-3,2,0,0,-1,0,3,-3,-2,-1,3,8,-1],[0,-2,-3,-3,-1,-3,-3,-3,-3,3,1,-2,1,0,-3,-1,0,-3,-1,5]]] ,[ "BLOSUM62" , [[4,-1,-2,-2,0,-1,-1,0,-2,-1,-1,-1,-1,-2,-1,1,0,-3,-2,0],[-1,5,0,-2,-3,1,0,-2,0,-3,-2,2,-1,-3,-2,-1,-1,-3,-2,-3],[-2,0,6,1,-3,0,0,0,1,-3,-3,0,-2,-3,-2,1,0,-4,-2,-3],[-2,-2,1,6,-3,0,2,-1,-1,-3,-4,-1,-3,-3,-1,0,-1,-4,-3,-3],[0,-3,-3,-3,9,-3,-4,-3,-3,-1,-1,-3,-1,-2,-3,-1,-1,-2,-2,-1],[-1,1,0,0,-3,5,2,-2,0,-3,-2,1,0,-3,-1,0,-1,-2,-1,-2],[-1,0,0,2,-4,2,5,-2,0,-3,-3,1,-2,-3,-1,0,-1,-3,-2,-2],[0,-2,0,-1,-3,-2,-2,6,-2,-4,-4,-2,-3,-3,-2,0,-2,-2,-3,-3],[-2,0,1,-1,-3,0,0,-2,8,-3,-3,-1,-2,-1,-2,-1,-2,-2,2,-3],[-1,-3,-3,-3,-1,-3,-3,-4,-3,4,2,-3,1,0,-3,-2,-1,-3,-1,3],[-1,-2,-3,-4,-1,-2,-3,-4,-3,2,4,-2,2,0,-3,-2,-1,-2,-1,1],[-1,2,0,-1,-3,1,1,-2,-1,-3,-2,5,-1,-3,-1,0,-1,-3,-2,-2],[-1,-1,-2,-3,-1,0,-2,-3,-2,1,2,-1,5,0,-2,-1,-1,-1,-1,1],[-2,-3,-3,-3,-2,-3,-3,-3,-1,0,0,-3,0,6,-4,-2,-2,1,3,-1],[-1,-2,-2,-1,-3,-1,-1,-2,-2,-3,-3,-1,-2,-4,7,-1,-1,-4,-3,-2],[1,-1,1,0,-1,0,0,0,-1,-2,-2,0,-1,-2,-1,4,1,-3,-2,-2],[0,-1,0,-1,-1,-1,-1,-2,-2,-1,-1,-1,-1,-2,-1,1,5,-2,-2,0],[-3,-3,-4,-4,-2,-2,-3,-2,-2,-3,-2,-3,-1,1,-4,-3,-2,11,2,-3],[-2,-2,-2,-3,-2,-1,-2,-3,2,-1,-1,-2,-1,3,-3,-2,-2,2,7,-1],[0,-3,-3,-3,-1,-2,-2,-3,-3,3,1,-2,1,-1,-2,-2,0,-3,-1,4]] ] ]};
 
 	var myJSON = JSON.stringify(myObj);
 	localStorage.setItem("submatJSON", myJSON);
 
 	var smtext = localStorage.getItem("submatJSON");
 	var smobj = JSON.parse(smtext);
-	var str = JSON.stringify(smobj);
+	var str = JSON.stringify(smobj);*/
 
-	console.log(str);
+	//console.log(str);
 
-	$scope.submatrixN = [];
-	$scope.submatrixP = [];
+      var smobj = {};
+      
+      /*var myJSON = JSON.stringify(smobj);
+    localStorage.setItem("submatJSON", myJSON);*/
 
-	for(var i=0;i<smobj.submatrixN.length;i++){
-		$scope.submatrixN.push(smobj.submatrixN[i][0]);
-	}
+    $scope.submatrixN = [];
+    $scope.submatrixP = [];
+    $scope.selmatrix = [];
 
-	for(var i=0;i<smobj.submatrixP.length;i++){
-		$scope.submatrixP.push(smobj.submatrixP[i][0]);
-	}
+    function selectNMatrix(){
+    var url1 = 'http://127.0.0.1:8080/api/getAllMatrices?matrixtype=' + 'Nucleotides';
 
-	$scope.selmatrix = [];
+    var selectreq = {
+        method: 'GET',
+        url: url1,
+        headers: {
+              'Content-Type': 'application/json',
+              'Access-Control-Request-Headers' : '*'
+        },
+      }
+
+      console.log(selectreq);
+
+      var request = $http(selectreq)
+        .then(function (response) {
+            console.log('success');
+            smobj = [];
+            for(var i =0;i<response.data.length;i++){
+              var temp1 = Object.keys(response.data[i])[1];
+              var obj1 = {};
+              obj1[temp1] = response.data[i][temp1];
+              smobj.push(obj1);
+            }
+            localStorage.setItem("submatnJSON", JSON.stringify(smobj));
+            /*var smtext1 = localStorage.getItem("submatnJSON");
+            var objn = JSON.parse(smtext1);*/
+             $scope.submatrixN = [];
+            for(var i =0;i<smobj.length;i++){
+              var str1 = Object.keys(smobj[i])[0];
+              $scope.submatrixN.push(str1);
+            }
+
+            
+          },function (response) {
+             console.log('error');
+          });
+
+     /*var smtext1 = localStorage.getItem("submatnJSON");
+    var objn = JSON.parse(smtext1);
+    
+    for(var i =0;i<objn.length;i++){
+        var str1 = Object.keys(objn[i])[0];
+        $scope.submatrixN.push(str1);
+      }*/
+
+    }
+
+    selectNMatrix();
+
+    function selectPMatrix(){
+
+    var url1 = 'http://127.0.0.1:8080/api/getAllMatrices?matrixtype=' + 'AminoAcids';
+
+    var selectreq = {
+        method: 'GET',
+        url: url1,
+        headers: {
+              'Content-Type': 'application/json',
+              'Access-Control-Request-Headers' : '*'
+        },
+      }
+
+      $http(selectreq)
+        .then(function (response) {
+            console.log('success');
+            smobj = response.data;
+            return response.data;
+          },function (response) {
+             console.log('error');
+          });
+
+        var request = $http(selectreq)
+        .then(function (response) {
+            console.log('success');
+            smobj = [];
+            for(var i =0;i<response.data.length;i++){
+              var temp1 = Object.keys(response.data[i])[1];
+              var obj1 = {};
+              obj1[temp1] = response.data[i][temp1];
+              smobj.push(obj1);
+            }
+            localStorage.setItem("submatpJSON", JSON.stringify(smobj));
+            /*var smtext2 = localStorage.getItem("submatpJSON");
+            var objp = JSON.parse(smtext2);*/
+            $scope.submatrixP = [];
+            for(var i =0;i<smobj.length;i++){
+              var str2 = Object.keys(smobj[i])[0];
+              $scope.submatrixP.push(str2);
+            }
+
+            
+             
+          },function (response) {
+             console.log('error');
+          });
+
+    
+
+   }
+
+   selectPMatrix();
+	
 
 	$scope.$watch('matrixtype', function(){
 			if($scope.matrixtype == "Nucleotides"){
-				$scope.selmatrix = $scope.submatrixN;
+        $scope.seltype = true;
 				$scope.regex = regexn;
 			}
 			else{
-				$scope.selmatrix = $scope.submatrixP;
+				$scope.seltype = false;
 				$scope.regex = regexp;
 			}
 
@@ -51,18 +151,25 @@ app.controller("initctrl", function($scope , $http){
 		if($scope.matrixname != ""){
 			
   			var mattype = null;
+        var smtext = ''; 
+        
 
   			if($scope.matrixtype == "Nucleotides")
-  				mattype = smobj.submatrixN;
+          smtext = localStorage.getItem("submatnJSON");
   			else
-  				mattype = smobj.submatrixP;
+  				smtext = localStorage.getItem("submatpJSON");
 
-  			for(var i=0;i<mattype.length;i++){
-  				if($scope.matrixname == mattype[i][0])
-  					$scope.matrix = mattype[i][1];
-  			}
+        var obj1 = JSON.parse(smtext);
+        var obj2 = {};
 
-  			console.log($scope.matrix[0][0]);
+
+        for(var i = 0;i<obj1.length;i++){
+          var strtemp = Object.keys(obj1[i])[0];
+          obj2[strtemp] = obj1[i][strtemp];
+        }
+
+        $scope.matrix = obj2[$scope.matrixname];
+  			
   		}
   	});
 
@@ -90,19 +197,16 @@ app.controller("initctrl", function($scope , $http){
   	$scope.parentObj = {matname:'' , match:'' , mismatch:''};
   	$scope.instatus = false;
   	$scope.inmessage = "";
+    $scope.alignoutput = "";
 
   	$scope.insert = function(oindex){
   		var tempmat = []; 
   		var n = 0;
-  		var temparr =  [];
-  		temparr.push($scope.parentObj.matname);
-
+  		
   		if($scope.matrixtype == "Nucleotides"){
-  			smobj.submatrixN.push(temparr);
   			n=4;
   		}
   		else{
-  			smobj.submatrixN.push(temparr);
   			n=20;
   		}
 
@@ -117,25 +221,49 @@ app.controller("initctrl", function($scope , $http){
   				tempmat.push(temp);
   			}
 
-  		var ind = 0;
-
-  			if($scope.matrixtype == "Nucleotides"){
-  				ind = smobj.submatrixN.length - 1;
-  				smobj.submatrixN[ind].push(tempmat);
-  			}
-  			else{
-  				ind = smobj.submatrixP.length - 1;
-  				$scope.submatrixP.push(tempmat);
-  			}
+  		var matobj = {};
+      matobj["matrixtype"] = $scope.matrixtype;
+      matobj['matrixname'] = $scope.parentObj.matname;
+  		matobj['matrixarray'] = tempmat;
   			
-  		console.log(smobj.submatrixN[ind][0][0]);
+  		//console.log(matobj);
 
-  		var myJSON = JSON.stringify(smobj);
-		localStorage.setItem("submatJSON", myJSON);
+  		/*var myJSON = JSON.stringify(smobj);
+		localStorage.setItem("submatJSON", myJSON);*/
 
-  		$scope.del(oindex);
-  		$scope.inmessage = "Inserted matrix successfully";
-  		$scope.instatus = true;
+    var matrixreq = {
+        method: 'POST',
+        url: 'http://127.0.0.1:8080/api/insertMatrix',
+        headers: {
+              'Content-Type': 'application/json',
+              'Access-Control-Request-Headers' : '*'
+        },
+        data: matobj
+      }
+
+      console.log(matrixreq);
+
+      $scope.inmessage = "";
+      $scope.del(oindex);
+      
+      
+      $http(matrixreq)
+        .then(function (response) {
+             $scope.inmessage = response['data'];
+          if($scope.inmessage == "Inserted successfully!"){
+             if($scope.matrixtype == "Nucleotides"){
+                selectNMatrix();
+            }else{
+                selectPMatrix();
+            }
+          }
+             console.log('success');
+          },function (response) {
+             console.log('error');
+          }); 
+
+        $scope.instatus = true;
+
   	}        
 
   	$scope.closeAlert = function(){
@@ -146,9 +274,9 @@ app.controller("initctrl", function($scope , $http){
   	$scope.initialize = function(){
   		//create JSON and invoke nodeJS
   		var reqdata =  { "sequence1" : $scope.seq1,
-      "sequence2" : $scope.seq2,
-      "alignment" : $scope.alignmethod,
-      "scorematrix" : $scope.matrix,
+      "sequence2" : $scope.seq2,      "scorematrix" : $scope.matrix,
+      "aligntype" : $scope.matrixtype,
+      "alignmethod" : $scope.alignmethod,
       "gap" : $scope.gap
     }
                 
@@ -156,6 +284,9 @@ app.controller("initctrl", function($scope , $http){
 
       var req = {
         method: 'POST',
+      "alignmethod" : $scope.alignmethod,
+      "aligntype" : $scope.matrixtype,
+
         url: 'http://127.0.0.1:3000/api/executeAlignment',
         headers: {
               'Content-Type': 'application/json',
@@ -164,14 +295,19 @@ app.controller("initctrl", function($scope , $http){
         data: reqdata
       }
 
-        console.log(req);
+      console.log(req);
 
-            $http(req)
-            .then(function (response) {
-              console.log('success');
-            },function (response) {
-              console.log('error')
-            });	
+      $http(req)
+        .then(function (response) {
+             console.log('success');
+             $scope.alignoutput = JSON.stringify(response.data);
+             console.log($scope.alignoutput);
+          },function (response) {
+             console.log('error');
+          });	
+
+        console.log($('#myform'));
+
   	}
 
 });
---
+++ AlgorithmServices/DBOperations.js
@@ -0,0 +1,103 @@
+var express        =        require("express");
+var bodyParser     =        require("body-parser");
+var app            =        express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.listen(8080,function(){
+  console.log("Started on PORT 8080");
+})
+
+
+
+app.get('/api/getAllMatrices',function(req,res){
+
+	var MongoClient1 = require('mongodb').MongoClient;
+	var assert1 = require('assert');
+	var ObjectId1 = require('mongodb').ObjectID;
+	var url1 = 'mongodb://tutor:tutor@ds115866.mlab.com:15866/seqaligntutor';
+	var collectionname = '';
+	
+	var selectDocuments = function(db, callback){
+		var cursor =db.collection(collectionname).find().toArray(function(err, docs) {
+    	assert1.equal(err, null);
+    	res.send(docs);
+    	console.log('Executed successfully');
+  		});
+
+	}
+
+	MongoClient1.connect(url1, function(err, db) {
+  		assert1.equal(null, err);
+  		if(req.query.matrixtype == "Nucleotides")
+  			collectionname = 'submatrixN';
+  		else
+  			collectionname = 'submatrixP';
+  		selectDocuments(db, function() {
+      	db.close();
+  		});
+  		
+	});
+
+});
+
+app.post('/api/insertMatrix',function(req,res){
+	
+	var MongoClient = require('mongodb').MongoClient;
+	var assert = require('assert');
+	var ObjectId = require('mongodb').ObjectID;
+	var url = 'mongodb://tutor:tutor@ds115866.mlab.com:15866/seqaligntutor';
+
+	var matobj = {};
+	matobj['_id'] = req.body.matrixname;
+	matobj[req.body.matrixname] = req.body.matrixarray;
+
+	console.log(req.body);
+
+	var collectionname = ''
+
+	if(req.body.matrixtype == "Nucleotides"){
+		collectionname = 'submatrixN';
+	}
+	else{
+		collectionname = 'submatrixP';
+	}
+
+	var insertDocument = function(db, callback) {
+		var count = db.collection(collectionname).find({'_id' : matobj['_id']}).toArray(function(err, result) {
+    		if (err) throw err;
+    		console.log(result);
+    		if(result.length == 0){
+ 				db.collection(collectionname).insertOne(matobj , function(err, result) {
+    			assert.equal(err, null);
+    			console.log("Inserted successfully!");
+    			res.send("Inserted successfully!");
+    			callback();
+ 				});
+ 			}
+ 			else{
+ 				res.send("Record already exists!");
+ 			}
+	
+  		});
+		
+	}
+
+	MongoClient.connect(url, function(err, db) {
+  		assert.equal(null, err);
+  		insertDocument(db, function() {
+      	db.close();
+  		});
+	});
+
+});
+
+
